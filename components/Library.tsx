803a47c3c21572608e1ec00934c98abccba83485
HarshSikriwal <19uec030@lnmiit.ac.in>
2023-09-16 17:33:04 +0530

    I have dont it. Stripe Integration Left

--- components/Library.tsx
+++ components/Library.tsx
@@ -7,6 +7,7 @@ import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
 import { Song } from "@/types";
 import MediaItem from "./MediaItem";
+import useOnPlay from "@/hooks/useOnPlay";
 
 interface LibraryProps {
   songs: Song[];
@@ -16,6 +17,8 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
   const authModal = useAuthModel();
   const uploadModal = useUploadModal();
   const { user } = useUser();
+
+  const onPlay = useOnPlay(songs);
 
   const onClick = () => {
     if (!user) {
@@ -38,7 +41,11 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
       </div>
       <div className="flex flex-col gap-y-2 mt-4 px-3">
         {songs.map((item) => (
-          <MediaItem data={item} key={item.id} onClick={() => {}} />
+          <MediaItem
+            data={item}
+            key={item.id}
+            onClick={(id: string) => onPlay(id)}
+          />
         ))}
       </div>
     </div>
