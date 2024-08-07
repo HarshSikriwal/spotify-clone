cb99eaf90e8785378c47bb282499ae96a51e200a
HarshSikriwal <harshsikriwal04@gmail.com>
2024-08-07 18:32:02 +0530

    feat: error message changed

--- app/account/error.tsx
+++ app/account/error.tsx
@@ -3,13 +3,11 @@
 import Box from "@/components/Box";
 
 const Error = () => {
-    return (
-        <Box className="h-full flex items-center justify-center">
-            <div className="text-neutral-400">
-                Something went wrong. Please try again!
-            </div>
-        </Box>
-    );
+  return (
+    <Box className="h-full flex items-center justify-center">
+      <div className="text-neutral-400">Something went wrong. Try again!</div>
+    </Box>
+  );
 };
 
 export default Error;
