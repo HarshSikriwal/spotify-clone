901f7de094fff3853dad9db97ede76c0e33ce169
HarshSikriwal <harshsikriwal04@gmail.com>
2024-08-07 18:18:14 +0530

    feat: error message changed

--- app/account/error.tsx
+++ app/account/error.tsx
@@ -5,9 +5,7 @@ import Box from "@/components/Box";
 const Error = () => {
     return (
         <Box className="h-full flex items-center justify-center">
-            <div className="text-neutral-400">
-                Something went wrong. Try again
-            </div>
+            <div className="text-neutral-400">Something went wrong.</div>
         </Box>
     );
 };
